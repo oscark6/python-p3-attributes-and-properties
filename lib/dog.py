a0f675fabdc6ea804d7b44b353364c4374462cd3
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-06-18 07:37:31 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,56 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    approved_breeds = [ 
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer"
+    ]
+
+    def __init__(self, name="Default Name", breed="Corgi"):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in Dog.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = None
+
+# Example usage:
+dog1 = Dog("Buddy", "Beagle")
+print(dog1.name)  
+print(dog1.breed)  
+
+dog2 = Dog("", "Corgi")  
+print(dog2.name)  
+print(dog2.breed)  
+
+dog3 = Dog("Fido", "Wolf")  
+print(dog3.name)  
+print(dog3.breed) 
+
+dog4 = Dog(123, "Pug")  
+print(dog4.name)  
+print(dog4.breed)  
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,66 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
+
+
+approved_jobs = [
+    "Admin",
+    "Customer Service",
+    "Human Resources",
+    "ITC",
+    "Production",
+    "Legal",
+    "Finance",
+    "Sales",
+    "General Management",
+    "Research & Development",
+    "Marketing",
+    "Purchasing"
+]
+
 class Person:
-    pass
+    def __init__(self, name="Default Name", job="General Management"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+            self._job = None
+
+# Example usage:
+person1 = Person("alice smith", "ITC")
+print(person1.name)   
+print(person1.job)   
+
+person2 = Person("", "Sales")  
+print(person2.name)   
+print(person2.job)   
+
+person3 = Person("Bob", "Chef")   
+print(person3.name)     
+print(person3.job)  
+
+person4 = Person(123, "Marketing")      
+print(person4.name)     
+print(person4.job)  
